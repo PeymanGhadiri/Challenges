e8c0015cc0104f7de6b5044d5072fb1ea9b0ef21
peyman <peyman.ghadiri@leap.com.au>
2017-11-22 16:23:36 +1100

    added first-factorial

---
+++ first-factorial/csharp/FirstFactorial/FirstFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class MainClass
+{
+    public static int FirstFactorial(int num)
+    {
+        var f = num;
+        for (int i = num-1; i >= 1; i--)
+        {
+            f = f * i;
+        }
+        return f;
+    }
+
+    static void Main()
+    {
+        while (true)
+        {
+            // keep this function call here
+            Console.WriteLine(FirstFactorial(Convert.ToInt32(Console.ReadLine())));
+        }
+    }
+
+}
