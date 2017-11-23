085137398639744ca736578626090d7f8105e0e4
peyman <peyman.ghadiri@leap.com.au>
2017-11-23 16:16:25 +1100

    3th and 4th chanllenge

---
+++ first-reverse/csharp/FirstReverse/FirstReverse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+class MainClass
+{
+    public static string FirstReverse(string str)
+    {
+        var sb=new StringBuilder();
+        for (int i = str.Length-1; i >=0; i--)
+        {
+            sb.Append(str[i]);
+        }
+        return sb.ToString();
+
+    }
+
+    static void Main()
+    {
+        // keep this function call here
+        while (true)
+        {
+            Console.WriteLine(FirstReverse(Console.ReadLine()));
+        }
+    }
+
+}
---
+++ letter-changes/csharp/LetterChanges/LetterChanges/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+class MainClass
+{
+    public static string LetterChanges(string str)
+    {
+        var sb=new StringBuilder();
+        for (int i = 0; i < str.Length; i++)
+        {
+
+            var c = Convert.ToInt32(str[i]);
+            if (IsLetter(c))
+            {
+                c += 1;
+                if (c == 97 || c == 101 || c == 105 || c == 111 || c==117)
+                    c -= 32;
+            }
+            sb.Append(Convert.ToChar(c));
+        }
+        return sb.ToString();
+    }
+
+    private static bool IsLetter(Int32 c)
+    {
+        return (c >= 65 && c <= 90) || (c>=97 && c<=122);
+    }
+    static void Main()
+    {
+        // keep this function call here
+        while (true)
+        {
+            Console.WriteLine(LetterChanges(Console.ReadLine()));
+        }
+    }
+
+}
