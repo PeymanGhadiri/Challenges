1c614f74b2e8c5b58d24acee672e2de585dbbaac
peyman <peyman.ghadiri@leap.com.au>
2017-11-30 15:49:20 +1100

    finished simple-adding

---
+++ simple-adding/csharp/SimpleAdding/SimpleAdding/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class MainClass
+{
+    public static int SimpleAdding(int num)
+    {
+        var t = 0;
+        for (int i = 1; i <= num; i++)
+        {
+            t += i;
+        }
+        return t;
+    }
+
+    static void Main()
+    {
+        // keep this function call here
+        while (true)
+        {
+            Console.WriteLine(SimpleAdding(Convert.ToInt32(Console.ReadLine())));
+        }
+    }
+
+}
